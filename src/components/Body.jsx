0579d6e915cc91a9bdf47ad80f47d14cf5dcc7bf
Kshitij S <kshitij.s@ril.com>
2024-02-20 01:02:48 +0530

    Added a filter button to get the top rated restros

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,15 +1,27 @@
 import ResCard from "./ResCard";
 import list from "../assets/ResList"
+import { useState } from "react";
 
 const Body = () => {
+  const [filteredRestros, setFilteredRestros] = useState(list);
     return (
       <div className="body">
+        <div className="filterBtn">
+          <button
+            className="topRatedBtn"
+            onClick={() => {
+              const filteredList = filteredRestros.filter(
+                (res) => res.data.avgRatingString >= 4.3
+              );
+              setFilteredRestros(filteredList)
+            }}
+          >
+            Top Rated Restaurants
+          </button>
+        </div>
         <div className="card-container">
-          {list.map((restaurant) => (
-            <ResCard 
-            key={restaurant.data.id}
-            {...restaurant.data}
-            />
+          {filteredRestros.map((restaurant) => (
+            <ResCard key={restaurant.data.id} {...restaurant.data} />
           ))}
         </div>
       </div>
