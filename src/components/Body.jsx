3d61ff588eac09addfce1450319213db47477376
Kshitij S <kshitij.s@ril.com>
2024-02-21 23:34:36 +0530

    Added dynamic data by calling an api and changed some css

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,21 @@
 import ResCard from "./ResCard";
-import list from "../assets/ResList";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Body = () => {
-  const [filteredRestros, setFilteredRestros] = useState(list);
+  const [filteredRestros, setFilteredRestros] = useState([]);
+
+  useEffect(()=>{
+    fetchData();
+  },[])
+
+  const fetchData = async () => {
+    const response = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.1045461&lng=73.00329959999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const jsonData = await response.json();
+    setFilteredRestros(jsonData?.data?.cards[4]?.card?.card.gridElements?.infoWithStyle?.restaurants);
+  };
+
 
   return (
     <div className="body">
@@ -12,7 +24,7 @@ const Body = () => {
           className="topRatedBtn"
           onClick={() => {
             const filteredList = filteredRestros.filter(
-              (res) => res.data.avgRatingString >= 4.3
+              (res) => res.info.avgRatingString >= 4.3
             );
             setFilteredRestros(filteredList);
           }}
@@ -21,8 +33,8 @@ const Body = () => {
         </button>
       </div>
       <div className="card-container">
-        {list.map((restaurant) => (
-          <ResCard key={restaurant.data.id} {...restaurant.data} />
+        {filteredRestros.map((restaurant) => (
+          <ResCard key={restaurant.info.id} {...restaurant.info} />
         ))}
       </div>
     </div>
