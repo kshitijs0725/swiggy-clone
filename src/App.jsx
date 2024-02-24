05cb2bfb3ac265a0149a73df313a724f03a6020d
Kshitij S <kshitij.s@ril.com>
2024-02-24 23:14:12 +0530

    Added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,41 @@
-import './App.css'
-import Header from './components/Header'
-import Body from './components/Body'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './App.css';
+import './index.css';
+import Header from './components/Header';
+import Body from './components/Body';
+import AboutPage from './components/About';
+import ContactPage from './components/Contact';
+
+const appRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/about',
+    element: <AboutPage />,
+  },
+  {
+    path: '/contact',
+    element: <ContactPage />,
+  },
+]);
 
 function App() {
   return (
     <>
-    <Header />
-    <Body />
+      <Header />
+      <Body />
     </>
-  )
+  );
 }
 
-export default App
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={appRouter}>
+      <App />
+    </RouterProvider>
+  </React.StrictMode>
+);
---
+++ src/components/Contact.jsx
@@ -0,0 +1,9 @@
+const ContactPage = () => {
+    return(
+        <div>
+            <h1>This is my contact page</h1>
+            <h2>To know more contact on helpline@swiggy.com</h2>
+        </div>
+    )
+}
+export default ContactPage;
